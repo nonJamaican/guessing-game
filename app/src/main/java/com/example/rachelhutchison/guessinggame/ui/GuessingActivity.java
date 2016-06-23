4509d106ca9eca336f43c254be7b4c775cb98f6a
Rachel Hutchison <Rachel.hutchison@i.tescobank.com>
2016-06-23 21:24:39 +0100

    passing through intent to guessing activity

--- app/src/main/java/com/example/rachelhutchison/guessinggame/ui/GuessingActivity.java
+++ app/src/main/java/com/example/rachelhutchison/guessinggame/ui/GuessingActivity.java
@@ -2,16 +2,32 @@ package com.example.rachelhutchison.guessinggame.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.example.rachelhutchison.guessinggame.R;
+import com.example.rachelhutchison.guessinggame.model.FanDuelPlayers;
 
 public class GuessingActivity extends AppCompatActivity {
 
     public static final String PLAYERS_DATA_EXTRA = "PLAYERS_DATA_EXTRA";
+    private FanDuelPlayers fanduelPlayersData;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guessing_game);
+        extractExtras();
+
+        if (fanduelPlayersData == null) {
+            displayErrorMessage();
+        }
+    }
+
+    private void extractExtras() {
+        fanduelPlayersData = (FanDuelPlayers) getIntent().getSerializableExtra(PLAYERS_DATA_EXTRA);
+    }
+
+    private void displayErrorMessage() {
+        Toast.makeText(this, R.string.network_error_message, Toast.LENGTH_LONG).show();
     }
 }
---
+++ app/src/test/java/com/example/rachelhutchison/guessinggame/ui/GuessingActivityTest.java
@@ -0,0 +1,22 @@
+package com.example.rachelhutchison.guessinggame.ui;
+
+import com.example.rachelhutchison.guessinggame.R;
+import com.example.rachelhutchison.guessinggame.RobolectricUnitTests;
+
+import org.junit.Test;
+import org.robolectric.Robolectric;
+import org.robolectric.shadows.ShadowToast;
+
+import static org.junit.Assert.*;
+
+public class GuessingActivityTest extends RobolectricUnitTests {
+
+    private GuessingActivity guessingActivity;
+
+    @Test
+    public void ifPlayerDataExtraMissingFromBundleWillShowMessageToUser() {
+        guessingActivity = Robolectric.buildActivity(GuessingActivity.class).create().visible().get();
+        assertEquals(guessingActivity.getString(R.string.network_error_message), ShadowToast.getTextOfLatestToast());
+
+    }
+}
